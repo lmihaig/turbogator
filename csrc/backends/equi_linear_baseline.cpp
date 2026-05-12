419c2799d78a7c95db2cca429c412f63be49e660
TheHummel <jannisalsbach@gmail.com>
2026-05-12 14:07:40 +0200

    include normalize parameter in equi_linear_baseline

--- csrc/backends/equi_linear_baseline.cpp
+++ csrc/backends/equi_linear_baseline.cpp
@@ -64,11 +64,16 @@ namespace tg
         }
     }
 
-    void equi_linear_baseline(const float *x, const float *weight, const float *bias, float *out, size_t n)
+    void equi_linear_baseline(const float *x, const float *weight, const float *bias, float *out, size_t n, bool normalize = true)
     {
         (void)x;
         (void)weight;
         (void)bias;
+
+        float basis[9][16][16];
+        memset(basis, 0, sizeof(basis));
+        _compute_pin_equi_linear_basis(normalize, basis);
+
         for (size_t i = 0; i < n; ++i)
         {
             out[i] = 0.0f;
