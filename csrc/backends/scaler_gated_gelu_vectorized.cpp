828f8112a2330f3a81c936e6ee8062e23f45864f
TheHummel <jannisalsbach@gmail.com>
2026-05-29 10:22:43 +0200

    add gated gelu  vectorized

---
+++ csrc/backends/scaler_gated_gelu_vectorized.cpp
@@ -0,0 +1,27 @@
+#include <cmath>
+#include <immintrin.h>
+
+#include "ops.hpp"
+
+namespace turbogator
+{
+
+    void scaler_gated_gelu_vectorized(const float *x, float *out, size_t n)
+    {
+        size_t num_mvs = n / 16;
+        for (size_t i = 0; i < num_mvs; ++i)
+        {
+            const float *mv_in = x + i * 16;
+            float *mv_out = out + i * 16;
+
+            float g = mv_in[0];
+            float inner = 0.7978845608028654f * (g + 0.044715f * g * g * g);
+            float gate = 0.5f * g * (1.0f + std::tanh(inner));
+
+            __m256 vgate = _mm256_set1_ps(gate);
+            _mm256_storeu_ps(mv_out, _mm256_mul_ps(_mm256_loadu_ps(mv_in), vgate));
+            _mm256_storeu_ps(mv_out + 8, _mm256_mul_ps(_mm256_loadu_ps(mv_in + 8), vgate));
+        }
+    }
+
+} // namespace turbogator
